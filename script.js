54accf8321a8e9b0d527718afed7eb88312181b5
Monke <120753922+L-spec875@users.noreply.github.com>
2025-11-20 12:03:36 -0600

    Update game URLs to use jsDelivr

--- script.js
+++ script.js
@@ -25,16 +25,18 @@ var HMR3_optionText = [
 var HMR3_optionValue = [
     "#", 
     "customlink",
-    "https://rawcdn.githack.com/L-spec875/Host/d8dfb5b32489b8d2973baa57af08cdcc7500f5ad/games/Eaglercraft%201.12.2.html",
-    "https://rawcdn.githack.com/L-spec875/Host/d8dfb5b32489b8d2973baa57af08cdcc7500f5ad/games/Pokemon%20Unbound%20(v2.1.1.1).zip",
-    "https://rawcdn.githack.com/L-spec875/Host/d8dfb5b32489b8d2973baa57af08cdcc7500f5ad/games/Pokemon%20GS%20Chronicles.zip",
-    "https://rawcdn.githack.com/L-spec875/Host/d8dfb5b32489b8d2973baa57af08cdcc7500f5ad/games/Pokemon%20-%20Emerald%20Version%20(USA%2C%20Europe).zip",
-    "https://rawcdn.githack.com/L-spec875/Host/d8dfb5b32489b8d2973baa57af08cdcc7500f5ad/games/Pokemon%20-%20LeafGreen%20Version%20(USA%2C%20Europe)%20(Rev%201).zip",
-    "https://rawcdn.githack.com/L-spec875/Host/d8dfb5b32489b8d2973baa57af08cdcc7500f5ad/games/3541%20-%20Pokemon%20Platinum%20Version%20(US)(XenoPhobia).7z",
-    "https://rawcdn.githack.com/L-spec875/Host/d8dfb5b32489b8d2973baa57af08cdcc7500f5ad/games/0434%20-%20New%20Super%20Mario%20Bros.%20(U)(Psyfer).7z",
-    "https://rawcdn.githack.com/L-spec875/Host/d8dfb5b32489b8d2973baa57af08cdcc7500f5ad/games/0168%20-%20Mario%20Kart%20DS%20(U)(SCZ).7z",
-    "https://rawcdn.githack.com/L-spec875/Host/d8dfb5b32489b8d2973baa57af08cdcc7500f5ad/games/Kirby%20%26%20The%20Amazing%20Mirror%20(USA).zip",
-    "https://rawcdn.githack.com/L-spec875/Host/d8dfb5b32489b8d2973baa57af08cdcc7500f5ad/games/Kirby%20-%20Nightmare%20in%20Dreamland%20(U)%20%5B!%5D.zip"
+    "https://cdn.jsdelivr.net/gh/L-spec875/Host@b09bfabe9484f8219f2a4106b78285b1c7e533a5/games/Eaglercraft%201.12.2.html",
+    "https://cdn.jsdelivr.net/gh/L-spec875/Host@b09bfabe9484f8219f2a4106b78285b1c7e533a5/games/Pokemon%20Unbound%20(v2.1.1.1).zip",
+    "https://cdn.jsdelivr.net/gh/L-spec875/Host@b09bfabe9484f8219f2a4106b78285b1c7e533a5/games/Pokemon%20GS%20Chronicles.zip",
+    "https://cdn.jsdelivr.net/gh/L-spec875/Host@b09bfabe9484f8219f2a4106b78285b1c7e533a5/games/Pokemon%20-%20Emerald%20Version%20(USA%2C%20Europe).zip",
+    "https://cdn.jsdelivr.net/gh/L-spec875/Host@b09bfabe9484f8219f2a4106b78285b1c7e533a5/games/Pokemon%20-%20LeafGreen%20Version%20(USA%2C%20Europe)%20(Rev%201).zip",
+    "https://cdn.jsdelivr.net/gh/L-spec875/Host@b09bfabe9484f8219f2a4106b78285b1c7e533a5/games/3541%20-%20Pokemon%20Platinum%20Version%20(US)(XenoPhobia).7z",
+    "https://cdn.jsdelivr.net/gh/L-spec875/Host@b09bfabe9484f8219f2a4106b78285b1c7e533a5/games/0434%20-%20New%20Super%20Mario%20Bros.%20(U)(Psyfer).7z",
+    "https://cdn.jsdelivr.net/gh/L-spec875/Host@b09bfabe9484f8219f2a4106b78285b1c7e533a5/games/0168%20-%20Mario%20Kart%20DS%20(U)(SCZ).7z",
+    "https://cdn.jsdelivr.net/gh/L-spec875/Host@b09bfabe9484f8219f2a4106b78285b1c7e533a5/games/Kirby%20%26%20The%20Amazing%20Mirror%20(USA).zip",
+    "https://cdn.jsdelivr.net/gh/L-spec875/Host@b09bfabe9484f8219f2a4106b78285b1c7e533a5/games/Kirby%20-%20Nightmare%20in%20Dreamland%20(U)%20%5B!%5D.zip",
+    
+    
 
 ];
 
@@ -70,7 +72,7 @@ var HMR3_SEGAMS = [
 ]
 
 var HMR3_HTML = [
-     "EaglerCraft 1.12 WASM"
+     "EaglerCraft 1.12 WASM",
     
 
 ]
