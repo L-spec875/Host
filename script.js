bd032caf359563eb035fef016d11ec7507262153
Monke <120753922+L-spec875@users.noreply.github.com>
2025-11-25 14:02:16 -0600

    Add Smash Flash 1 and Too many types

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ var HMR3_optionText = [
     "EaglerCraft 1.12 WASM",
     "Pokemon Unbound",
     "Pokemon GS Chronicles",
+    "Pokemon Too Many Types",
     "Pokemon Emerald",
     "Pokemon Leafgreen",
     "Pokemon Platinum",
@@ -22,7 +23,7 @@ var HMR3_optionText = [
     "Kirby & The Amazing Mirror",
     "Kirby: Nightmare in Dreamland",
     "Bad Time Simulator",
-    "Super Smash Flash 2"
+    "Super Smash Flash"
 ];
 var HMR3_optionValue = [
     "#", 
@@ -30,6 +31,7 @@ var HMR3_optionValue = [
     "https://l-spec875.github.io/htmls.github.io/index.html",
     "https://cdn.jsdelivr.net/gh/L-spec875/Host@b09bfabe9484f8219f2a4106b78285b1c7e533a5/games/Pokemon%20Unbound%20(v2.1.1.1).zip",
     "https://cdn.jsdelivr.net/gh/L-spec875/Host@b09bfabe9484f8219f2a4106b78285b1c7e533a5/games/Pokemon%20GS%20Chronicles.zip",
+    "https://cdn.jsdelivr.net/gh/L-spec875/Host@1ca1067c43d9f27e29783d60494e4e9af1dd4f29/games/Too%20many%20types.zip",
     "https://cdn.jsdelivr.net/gh/L-spec875/Host@b09bfabe9484f8219f2a4106b78285b1c7e533a5/games/Pokemon%20-%20Emerald%20Version%20(USA%2C%20Europe).zip",
     "https://cdn.jsdelivr.net/gh/L-spec875/Host@b09bfabe9484f8219f2a4106b78285b1c7e533a5/games/Pokemon%20-%20LeafGreen%20Version%20(USA%2C%20Europe)%20(Rev%201).zip",
     "https://cdn.jsdelivr.net/gh/L-spec875/Host@b09bfabe9484f8219f2a4106b78285b1c7e533a5/games/3541%20-%20Pokemon%20Platinum%20Version%20(US)(XenoPhobia).7z",
@@ -38,14 +40,14 @@ var HMR3_optionValue = [
     "https://cdn.jsdelivr.net/gh/L-spec875/Host@b09bfabe9484f8219f2a4106b78285b1c7e533a5/games/Kirby%20%26%20The%20Amazing%20Mirror%20(USA).zip",
     "https://cdn.jsdelivr.net/gh/L-spec875/Host@b09bfabe9484f8219f2a4106b78285b1c7e533a5/games/Kirby%20-%20Nightmare%20in%20Dreamland%20(U)%20%5B!%5D.zip",
     "https://l-spec875.github.io/html2.github.io/index.html",
-    "https://cdn.jsdelivr.net/gh/L-spec875/Host@1371c984b59458896a4b15003ec3d33482dc80f4/games/SSF2.swf",
+    "https://cdn.jsdelivr.net/gh/L-spec875/Host@1ca1067c43d9f27e29783d60494e4e9af1dd4f29/games/SuperSmash.swf",
     
     
 
 ];
 
 var flashgames = [
-"Super Smash Flash 2",
+"Super Smash Flash",
 ]
 
 var HMR3_nds = [
